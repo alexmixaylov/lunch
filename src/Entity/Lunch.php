d5a4ab3e9bfac3496277349ab6bfe52de74559ac
Alexmixaylov <alexmixaylov@gmail.com>
2020-05-01 15:50:36 +0300

    add relationship OneToMany between Dish and Lunch

---
+++ src/Entity/Lunch.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LunchRepository")
+ */
+class Lunch
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Dish", mappedBy="lunch")
+     */
+    private $dishes;
+
+
+    public function __construct()
+    {
+        $this->dishes = new ArrayCollection();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Dish[]
+     */
+    public function getDishes(): Collection
+    {
+        return $this->dishes;
+    }
+
+    public function addDish(Dish $dish): self
+    {
+        if (!$this->dishes->contains($dish)) {
+            $this->dishes[] = $dish;
+            $dish->setLunch($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDish(Dish $dish): self
+    {
+        if ($this->dishes->contains($dish)) {
+            $this->dishes->removeElement($dish);
+            // set the owning side to null (unless already changed)
+            if ($dish->getLunch() === $this) {
+                $dish->setLunch(null);
+            }
+        }
+
+        return $this;
+    }
+
+}
