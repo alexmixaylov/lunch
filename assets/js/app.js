89c0096eed74969177afd1f13e1a944d2fdf5397
Alexmixaylov <alexmixaylov@gmail.com>
2020-05-02 12:53:50 +0300

    Webpac Encore was installed and now preparing other parts

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+import Routes from './routes.js';
+
+import App from './views/App';
+
+Vue.use(Vuetify);
+
+const app = new Vue({
+    el: '#app',
+    router: Routes,
+    render: h => h(App),
+});
+
+export default app;
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
