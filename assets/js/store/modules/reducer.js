290e9a9d8b34070d0ab5dee921d6dd757e5b56f9
Alexmixaylov <alexmixaylov@gmail.com>
2020-05-11 11:24:35 +0300

    reducer JS  was added into Store

---
+++ assets/js/store/modules/reducer.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+export default {
+    namespaced: true,
+    state: {
+        orders: [],
+        dishes:[],
+        clients:[]
+    },
+    getters: {
+        getOrders: state =>{
+            return state.orders
+        },
+        getDishes: state =>{
+            return state.dishes
+        },
+    },
+    mutations: {
+        addOrders(state, payload){
+            state.orders = payload
+        },
+        addDishes(state, payload){
+            state.dishes = payload
+        }
+    },
+    actions: {
+        // this module ONLY FOR READING aggregated data. ONLY LOAD DATA FROM SERVER
+        loadOrdersByDate({commit}, payload){
+            console.log(payload)
+            axios
+                .get('/orders/date/'+ payload)
+                .then(response => {
+                    console.log(response)
+                    commit('addOrders', response.data)
+                })
+                .catch(error => {console.log(error)})
+        },
+        loadDishesByDate({commit}, payload){
+            console.log(payload)
+            axios
+                .get('/orders/date/'+ payload)
+                .then(response => {
+                    console.log(response)
+                    commit('addOrders', response.data)
+                })
+                .catch(error => {console.log(error)})
+        }
+    }
+}
